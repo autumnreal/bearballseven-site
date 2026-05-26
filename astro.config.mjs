0c08a6145bc91f8727c9bc58b0ba43a567a90d01
Autumnreal <bearball.seven@gmail.com>
2026-05-26 14:55:48 +0800

    Change primary domain to bearballseven.com

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,6 @@ import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: 'https://www.bearballseven.com',
+  site: 'https://bearballseven.com',
   integrations: [sitemap()]
 });
